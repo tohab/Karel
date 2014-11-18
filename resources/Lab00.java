89c017a75bf03b28a46cdc644928b4fff6d5f622
tohab <rohan_prasad@asl.org>
2014-11-18 08:31:18 +0000

    Worked on Lab00

--- resources/Lab00.java
+++ resources/Lab00.java
@@ -1,1 +1,17 @@
 import edu.fcps.karel2.Display;
+import edu.fcps.karel2.Robot;
+
+public class Lab00 {
+	public static void main(String arg[]) {
+		Display.openWorld("maps/first.map");
+		Display.setSize(10, 10);
+		Display.setSpeed(5);
+		
+		Robot karel = new Robot();
+		
+		karel.move();
+		karel.pickBeeper();
+		karel.move();
+		
+	}
+}
